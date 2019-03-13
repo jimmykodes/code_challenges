49d5a113e4c483bf98d88d7e3903e81e944bf9b3
gibsonbailey <gibson.bailey@live.com>
2019-03-13 01:23:56 -0700

    shift_cypher sub

---
+++ 03_11_shift_cypher/bailey_submission.py
@@ -0,0 +1,13 @@
+def encode(plaintext, shift):
+    print(plaintext)
+    b = bytearray(plaintext, 'utf-8')
+    for i in range(len(plaintext)):
+        c = b[i]
+        if c > 64 and c < 91:
+            b[i] = (c - 65 + shift) % 26 + 65
+        elif c > 96 and c < 123:
+            b[i] = (c - 97 + shift) % 26 + 97
+    return b.decode()
+
+def decode(cypher_text, shift):
+    return encode(cypher_text, -shift)
